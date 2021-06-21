4aa4ee3f6c19590dc531e1a8e16b3b2f84e122ac
= <=>
2021-06-21 00:29:40 -0300

    Finalização da aula do dia 20/06

--- letmeask/src/services/firebase.ts
+++ letmeask/src/services/firebase.ts
@@ -5,14 +5,14 @@ import 'firebase/database';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAokxuwReu7kxVkmRjWB6qIiv6M8vbkoFA",
-    authDomain: "letmeask-72fbf.firebaseapp.com",
-    databaseURL: "https://letmeask-72fbf-default-rtdb.firebaseio.com",
-    projectId: "letmeask-72fbf",
-    storageBucket: "letmeask-72fbf.appspot.com",
-    messagingSenderId: "977639468727",
-    appId: "1:977639468727:web:30587273a40d59032a6732",
-    measurementId: "G-F8SKS192R4"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_ALPH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATEBASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_SENDER_IF,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
