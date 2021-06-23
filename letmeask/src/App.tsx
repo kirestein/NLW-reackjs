90b84cb78a864f5b26dfef13ce5dd4f7053949fb
= <=>
2021-06-23 09:22:40 -0300

    Modificando arquivos

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,8 +1,9 @@
 import { Button } from './components/Button'
+import { Home } from './pages/Home';
 
 function App() {
   return (
-    <Button text="Bom dia"/>
+    <Home />
   )}
 
 export default App;
